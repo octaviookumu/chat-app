4c4001f30e87576216ba286399fe3cab769b5063
Octavian Okumu <octaviookumu@gmail.com>
2021-04-12 12:26:44 +0300

    First commit

---
+++ app/Models/ChatMessage.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ChatMessage extends Model
+{
+    use HasFactory;
+
+    public function room(){
+        return $this->hasOne(ChatRoom::class, 'id', 'chat_room_id');
+        // return $this->hasMany('App\Models\ChatRoom', 'id', 'chat_room_id');
+    }
+
+    public function user(){
+        return $this->hasOne(User::class, 'id', 'user_id');
+        // return $this->hasMany('App\Models\User', 'id', 'user_id');
+    }
+}
---
+++ resources/js/Pages/Chat/ChatRoomSelection.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="grid grid-cols-2">
+        <div class="font-bold text-xl">
+            {{ selected.name }} Chat
+        </div>
+        <div>
+            <!-- onchange we fire the roomchanged event and pass selected to event launcher in parent -->
+            <select
+                v-model="selected"
+                @change="$emit('roomchanged', selected)"
+                class="float-right"
+            >
+                <option
+                    v-for="(room, index) in rooms"
+                    :key="index"
+                    :value="room"
+                    >
+                        {{ room.name }}
+                    </option>
+            </select>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ChatRoomSelection',
+        props: ["rooms", "currentRoom"],
+        data: function(){
+            return{
+                selected: ''
+            }
+        },
+        created(){
+            this.selected = this.currentRoom;
+        }
+    }
+</script>
